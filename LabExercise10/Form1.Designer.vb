000d3281187a3501ee321f6a109b487242d0acd5
marionravensapon <marionsapon.02@gmail.com>
2025-11-04 09:39:00 +0800

    Add project files.

---
+++ LabExercise10/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonWrite = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ButtonReadPerLine = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ButtonWrite
+        '
+        Me.ButtonWrite.Location = New System.Drawing.Point(475, 128)
+        Me.ButtonWrite.Name = "ButtonWrite"
+        Me.ButtonWrite.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonWrite.TabIndex = 0
+        Me.ButtonWrite.Text = "Write"
+        Me.ButtonWrite.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(332, 174)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(312, 212)
+        Me.ListBox1.TabIndex = 1
+        '
+        'ButtonReadPerLine
+        '
+        Me.ButtonReadPerLine.Location = New System.Drawing.Point(587, 128)
+        Me.ButtonReadPerLine.Name = "ButtonReadPerLine"
+        Me.ButtonReadPerLine.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonReadPerLine.TabIndex = 2
+        Me.ButtonReadPerLine.Text = "Read"
+        Me.ButtonReadPerLine.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(313, 131)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(225, 133)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Enter a number:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(690, 128)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Sort"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.ButtonReadPerLine)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ButtonWrite)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonWrite As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ButtonReadPerLine As Button
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ LabExercise10/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+
+Public Class Form1
+    Private Sub ButtonWrite_Click(sender As Object, e As EventArgs) Handles ButtonWrite.Click
+        Try
+            Dim filePath As String = "sample.txt" ' File Location
+
+            Using writer As New StreamWriter(filePath, True) 'True to apppend
+                writer.WriteLine("Hello, this is a test file!")
+                writer.WriteLine("Second line of the text.")
+            End Using
+
+            MessageBox.Show("Data written successfully.", "Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonReadPerLine.Click
+        Dim filepath As String = "sample.txt"
+        Using reader As New StreamReader(filepath)
+            Dim content As String = reader.ReadToEnd()
+            MessageBox.Show("File content:" + content)
+        End Using
+    End Sub
+
+    Private Sub ButtonReadPerLIne_Click(sender As Object, e As EventArgs) Handles ButtonReadPerLIne.Click
+        ListBox1.Items.Clear()
+        Dim filepath As String = "sample.txt"
+        Using reader As New StreamReader(filepath)
+            Dim Line As String
+            Line = reader.ReadLine()
+            While (Line IsNot Nothing)
+                ListBox1.Items.Add(Line)
+                Line = reader.ReadLine()
+            End While
+        End Using
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
